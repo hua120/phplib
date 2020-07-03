bb913c885985559619bd7329c7b0d42ae0c3c592
hua120 <31441796+hua120@users.noreply.github.com>
2020-07-03 21:53:53 +0800

    Update http.php

--- http.php
+++ http.php
@@ -4,13 +4,13 @@
  * php通过socket发送http GET请求
  */
 
-$fp  =  fsockopen ( "www.baidu.com" ,  80 ,  $errno ,  $errstr ,  30 );
+$fp  =  fsockopen ( "ssl://www.163.com" ,  443 ,  $errno ,  $errstr ,  30 );
 // var_dump($fp);exit;	文件句柄
 if (! $fp ) {
     echo  " $errstr  ( $errno )<br />hello\n" ;
 } else {
      $out  =  "GET / HTTP/1.1\r\n" ;
-     $out  .=  "Host: www.baidu.com\r\n" ;
+     $out  .=  "Host: www.163.com\r\n" ;
      $out  .=  "Connection: Close\r\n\r\n" ;
      fwrite ( $fp ,  $out );
     while (! feof ( $fp )) {
